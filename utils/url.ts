354f79848712edb7d0c5712c9b6cfe0ec69607d9
riaz37 <riaz37.ipe@gmail.com>
2024-12-03 18:36:56 +0600

    dunamic lnk

--- utils/url.ts
+++ utils/url.ts
@@ -1,7 +1,7 @@
 export const getBaseUrl = () => {
-  if (typeof window !== 'undefined') return ''; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  if (process.env.NEXT_PUBLIC_APP_URL) return process.env.NEXT_PUBLIC_APP_URL;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
 };
 
 export const getAbsoluteUrl = (path: string) => {
