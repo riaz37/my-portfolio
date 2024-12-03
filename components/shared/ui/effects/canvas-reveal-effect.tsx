5e75acc71da5fcf3eedfc28721fc55b943e5bcda
riaz37 <riaz37.ipe@gmail.com>
2024-12-03 21:06:04 +0600

    done

---
+++ components/shared/ui/effects/canvas-reveal-effect.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const CanvasRevealEffect: React.FC = () => {
+  // Implement your canvas reveal effect component here
+  return <div>Canvas Reveal Effect</div>;
+};
